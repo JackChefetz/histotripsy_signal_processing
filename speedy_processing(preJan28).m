194866d1240b78cb84c5abf31fb4a2fc932c17e6
JackChefetz <jchefetz@uchicago.edu>
2025-02-03 14:58:12 -0600

    JackFeb3

--- speedy_processing(preJan28).m
+++ speedy_processing(preJan28).m
@@ -2,13 +2,10 @@
 
 
 %%% ttp = time to next pulse
-function [ttp]= speedy_processing(UFData, Trans, RData)
+function [ttp]= speedy_processing(Trans)
 
 
 %% loading relevant data files
-
-%y = UFdata;
-
 Trans = Zzz.Trans;
 P = Zzz.P;
 Receive3 = Zzz.Receive3;
@@ -33,12 +30,14 @@ w2 = interp1((1:length(tw2))/tFs,tw2,1/Fs:1/Fs:length(tw2)/tFs);
 w2 = [zeros(1,length(w2)) w2]; % padding extra zeros
 w2 = w2/max(abs(w2));
 
+y = load(filename);
+
 % Determine number of points associated with individual waveform
-twfm = RData(:, 64); % Averaged RF data collection
+twfm = y.RData(:, 64); % Averaged RF data collection
 numZeros = find(flipud(twfm) ~= 0, 1, 'first')-1;  % Lenght of all RF data without additional buffer
 ptsd = int16((length(twfm)-numZeros)/P.numAcqs); % Points per frame
 frame_no = 1;
-x = RData((frame_no-1)*ptsd+(1:ptsd),:);
+x = y.RData((frame_no-1)*ptsd+(1:ptsd),:);
 
 %% Determine absolute time
 time = (1:double(ptsd))*(1/Fs) + 2*Receive3(64).startDepth/(Trans.frequency*1e6);
@@ -46,7 +45,7 @@ time = (1:double(ptsd))*(1/Fs) + 2*Receive3(64).startDepth/(Trans.frequency*1e6)
 %% Apply matched filter to waveforms
 fwfm = zeros(ptsd, lenData);
 for idx = 1:lenData
-    fwfm(:, idx) = conv2(RData((idx-1)*ptsd+(1:ptsd),64)', fliplr(w2),'same')';
+    fwfm(:, idx) = conv2(y.RData((idx-1)*ptsd+(1:ptsd),64)', fliplr(w2),'same')';
 end
 
 
@@ -67,4 +66,3 @@ end
 params = speedy_power_fit((1:lenData)', intGS'/intGS(1));
 ttp = params(1) * (1:lenData)'.^params(2);
 
-2+3
