1e41b4cd1a81886768e5ede89ef4d77d131d3836
JackChefetz <jchefetz@uchicago.edu>
2025-02-27 17:53:07 -0600

    feb25 results

---
+++ 2_25_results/gel0int.py
@@ -0,0 +1,12 @@
+# 1 sample of gel at 0V to show increasing intensity trend
+
+ttp =[1.0000,
+    2.1741,
+    2.1302,
+    1.9601,
+    2.0552,
+    2.0526,
+    2.0722,
+    2.0672,
+    2.0726,
+    2.1935]
